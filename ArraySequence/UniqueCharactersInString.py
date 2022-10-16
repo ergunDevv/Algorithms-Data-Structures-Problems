c07e993e422f37ae21213aacbd10f5dcf7e5e86a
ergunDevv <ergncyln2127088@gmail.com>
2022-10-16 11:11:11 +0300

    Unique Characters In String Problem Solution

--- ArraySequence/UniqueCharactersInString.py
+++ ArraySequence/UniqueCharactersInString.py
@@ -1,3 +1,18 @@
 #PROBLEM : Given a string, determine if it is compreised of all unique characters.
 # For example, the string 'abcde' has all unique characters and should return True
 # The string 'aabcde' contains duplicate characters and should return false
+
+
+def uniqueCharacter(s):
+    
+    i=1
+    length=len(s)
+    while i<length:
+        if s[i] == s[i-1]:
+            return False
+        i+=1
+    return True
+
+
+a = uniqueCharacter("")
+print(a)
