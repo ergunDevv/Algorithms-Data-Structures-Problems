1e7d7bff84767bd26fe793d797af19e7ccfa7cef
ergunDevv <ergncyln2127088@gmail.com>
2022-10-19 11:11:11 +0300

    Sentence Reversal Problem Second Solution

--- ArraySequence/SentenceReversal.py
+++ ArraySequence/SentenceReversal.py
@@ -17,3 +17,29 @@ def sentenceReversal(str):
 
 a=sentenceReversal("  Hi John,   are you ready to go?  ")
 print(a)
+
+
+##!--------------------------------------------------------------------------
+# Second Solution
+
+def sentenceReversal2(str):
+    words=[]
+    length= len(str)
+    spaces= [' ']
+
+    i=0
+
+    while i < length:
+        
+        if str[i] not in spaces:
+            word_start=i
+
+            while i <length and str[i] not in spaces:
+                i+=1
+
+            words.append(str[word_start:i])
+        i+=1
+    return ' '.join(reversed(words))
+
+b=sentenceReversal2("  Hi John,   are you ready to go?  ")
+print(b)
