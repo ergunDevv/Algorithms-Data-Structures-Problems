27c2d0dd538930066545b9f0f96c8745fc6a285c
ergunDevv <ergncyln2127088@gmail.com>
2022-10-21 11:11:11 +0300

    Sentence Reversal Problem Solution

--- ArraySequence/SentenceReversal.py
+++ ArraySequence/SentenceReversal.py
@@ -2,8 +2,15 @@
 # Given: 'This is the best'
 # Return : 'best the is This'
 
+def sentenceReversal(str):
+    str=str.strip().split()
+    reversedString=""
+    for word in reversed(str):
+        reversedString+=word
+        reversedString+=" "
+
+    return reversedString
 
 
-
-
-
+a=sentenceReversal("  Hi John,   are you ready to go?  ")
+print(a)
