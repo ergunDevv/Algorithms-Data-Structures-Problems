ae0629e276472e012def6052a9ddf23932a9ec3b
ergunDevv <ergncyln2127088@gmail.com>
2022-10-22 11:11:11 +0300

    LargestContinuousSum Solution Comments

--- ArraySequence/LargestContinuousSum.py
+++ ArraySequence/LargestContinuousSum.py
@@ -3,15 +3,19 @@
 # OUTPUT : 29
 
 def large_cont_sum(Arr1):
+    # Edge case check
     if(len(Arr1))==0: return 0
-    
+    # Creating 2 variables for algorithm
     max_sum = current_sum = Arr1[0]
 
     for num in Arr1[1:]:
+        # Down below checking is current_sum+num higher than num and 
+        # assign the higher one to current_sum
         current_sum = max(current_sum+num,num)
-        print(current_sum)
+        # Then checking again is current_sum is higher than max_sum if it is 
+        # assigning current_sum value to the max_sum 
         max_sum = max(max_sum,current_sum)
-
+    # Return the max_sum
     return max_sum
 
 
