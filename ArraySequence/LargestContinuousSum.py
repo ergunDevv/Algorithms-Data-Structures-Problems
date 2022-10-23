3527aa4aba7bf511c0b3593377bef3a4b57ae1f4
ergunDevv <ergncyln2127088@gmail.com>
2022-10-23 11:11:11 +0300

    LargestContinuousSum Solution

--- ArraySequence/LargestContinuousSum.py
+++ ArraySequence/LargestContinuousSum.py
@@ -2,3 +2,18 @@
 # INPUT : large_cont_sum([1,2,-1,3,4,10,10,-10,-1])
 # OUTPUT : 29
 
+def large_cont_sum(Arr1):
+    if(len(Arr1))==0: return 0
+    
+    max_sum = current_sum = Arr1[0]
+
+    for num in Arr1[1:]:
+        current_sum = max(current_sum+num,num)
+        print(current_sum)
+        max_sum = max(max_sum,current_sum)
+
+    return max_sum
+
+
+a = large_cont_sum([1, 2, -1, 3, 4, -1])
+print(a)
