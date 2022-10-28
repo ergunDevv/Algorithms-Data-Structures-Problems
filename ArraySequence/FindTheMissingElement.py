6c55fce65198493aa511d1d52cb91a5cfd3b01c3
ergunDevv <ergncyln2127088@gmail.com>
2022-10-28 11:11:11 +0300

    FindTheMissingElement Comments

--- ArraySequence/FindTheMissingElement.py
+++ ArraySequence/FindTheMissingElement.py
@@ -1,10 +1,14 @@
 def FindTheMissingElement(Arr1,Arr2):
+    # Defining sum of Elements variable
     sumOfElements=0
+    # First adding every number to sumOfElements
     for number in Arr1:
-        sumOfElements+=number
+        sumOfElements+=number;
+    # Then subtracting every number in sumOfElements
     for number in Arr2:
         sumOfElements-=number
-
+    # And returning the value of sumOfElements. Cause after adding and subtracting there will be 
+    # just one number that missing. 
     return  sumOfElements
 a=FindTheMissingElement([9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1])
 print(a)
