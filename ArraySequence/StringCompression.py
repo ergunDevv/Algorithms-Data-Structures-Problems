5718840b47e5648ceeae48c2dbd36476eea1841e
ergunDevv <ergncyln2127088@gmail.com>
2023-03-01 21:59:51 +0300

    String Compression ProblemSolution

--- ArraySequence/StringCompression.py
+++ ArraySequence/StringCompression.py
@@ -3,3 +3,24 @@
 # For instance, it is okay for 'AAB' to return 'A2B1' even though this technically 
 # takes more space. The function should also be case sensetive, so that a string 
 # 'AAAaaa' returns 'A3a3'
+
+def stringCompression(stringForComp):
+    stringForComp = stringForComp.replace(" ", "")
+    numberOfLetter={}
+    stringForReturn=""
+    for letter in stringForComp:
+        print(letter)
+        if numberOfLetter.get(letter):
+           numberOfLetter[letter]+=1
+        else:
+            numberOfLetter[letter]=1
+
+    for (key,value) in numberOfLetter.items():
+        stringForReturn+=key
+        stringForReturn+=str(value)
+
+
+    return stringForReturn
+
+a=stringCompression("AAABBBBcDDDddhB")
+print(a)
