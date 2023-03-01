7a4f6b6198e525358b7d5868257d9cca93decb02
ergunDevv <ergncyln2127088@gmail.com>
2023-03-01 21:43:03 +0300

    String Compression Problem

---
+++ ArraySequence/StringCompression.py
@@ -0,0 +1,5 @@
+# PROBLEM: Given a string in the form 'AAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2E4'
+# For this problem, you can falsely "compress" strings of single of double letters.
+# For instance, it is okay for 'AAB' to return 'A2B1' even though this technically 
+# takes more space. The function should also be case sensetive, so that a string 
+# 'AAAaaa' returns 'A3a3'
