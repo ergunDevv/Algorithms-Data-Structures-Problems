cafce77a41b5b1a1949efa3e51c4767076805f60
ergunDevv <ergncyln2127088@gmail.com>
2023-03-02 22:06:37 +0300

    Balanced ParanthesesCheckProblem Solution Added

--- StacksQueuesAndDequesProblems/BalancedParanthesesCheckProblem.py
+++ StacksQueuesAndDequesProblems/BalancedParanthesesCheckProblem.py
@@ -5,3 +5,31 @@
 #no spaces words or numbers.
 # As a reminder, balanced parentheses require every opening parenthesis to be closed in the reverse order opened.
 # For example '([])' is balanced but '([)]' is not. You can assume the input string has no spaces 
+
+
+def balance_check(s):
+
+    if len(s)%2 !=0: return False
+
+    openings = set('({[')
+    matches = set([('(',')'),('{','}'),('[',']')])
+
+    stack = []
+    
+    for parantheses in s :
+        if parantheses in openings:
+            stack.append(parantheses)
+        else:
+            if len(stack)==0:
+                return False
+            last_open=stack.pop()
+            if(last_open,parantheses) not in matches:
+                return False
+    return len(stack) == 0
+            
+
+
+
+print(balance_check("()"))
+print(balance_check("())[]"))
+
