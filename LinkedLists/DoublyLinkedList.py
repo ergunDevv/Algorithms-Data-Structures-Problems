89d19c324f5ffbe913cd2ea2a1fd75c7e02c67c3
ergunDevv <ergncyln2127088@gmail.com>
2023-03-04 11:11:11 +0300

    Doubly Linked List Implementation

---
+++ LinkedLists/DoublyLinkedList.py
@@ -0,0 +1,18 @@
+class DoublyLinkedListNode(object):
+    def __init__(self,value) :
+        self.value = value
+        self.prev_node=None
+        self.next_node=None
+
+a=DoublyLinkedListNode(1)
+b=DoublyLinkedListNode(2)
+c=DoublyLinkedListNode(3)
+
+
+a.next_node=b
+b.prev_node=a
+b.next_node=c
+c.prev_node=b
+print(c.prev_node.value)
+print(b.prev_node.value)
+
