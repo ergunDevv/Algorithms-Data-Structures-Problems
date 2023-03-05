63956f62d29b135bde169eec98cf36bf5be7301a
ergunDevv <ergncyln2127088@gmail.com>
2023-03-05 11:11:11 +0300

    Linked List Explanation Comment Lines added

--- LinkedLists/SinglyLinkedList.py
+++ LinkedLists/SinglyLinkedList.py
@@ -1,12 +1,14 @@
 class Node(object):
     def __init__(self,value):
+        # Creating node for linked list that stores value and nextNode
         self.value=value
         self.nextNode=None
 
-
+# Creating Nodes
 a=Node(1)
 b=Node(2)
 c=Node(3)
+# Linking nodes to each other.
 a.nextNode=b
 b.nextNode=c
 print(a.nextNode.value)
