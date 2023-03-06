1de9b2b61c3d1c6da53556abdd4697fca1986a04
ergunDevv <ergncyln2127088@gmail.com>
2023-03-07 00:07:50 +0300

    Doubly Links Implementation Explanation Comments added

--- LinkedLists/DoublyLinkedList.py
+++ LinkedLists/DoublyLinkedList.py
@@ -1,14 +1,15 @@
 class DoublyLinkedListNode(object):
     def __init__(self,value) :
+        # Creating Doubly Linked List Node Object
         self.value = value
         self.prev_node=None
         self.next_node=None
-
+# Creating Doubly Linked List Node
 a=DoublyLinkedListNode(1)
 b=DoublyLinkedListNode(2)
 c=DoublyLinkedListNode(3)
 
-
+# Defining the links between nodes.
 a.next_node=b
 b.prev_node=a
 b.next_node=c
