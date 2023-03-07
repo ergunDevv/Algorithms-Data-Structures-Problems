783552319f274003692fed1a62f1d43a0a6e013a
ergunDevv <ergncyln2127088@gmail.com>
2023-03-07 15:55:55 +0300

    SinglyLinkedListCylceCheck Problem

---
+++ LinkedLists/SinglyLinkedListCycleCheckProblem.py
@@ -0,0 +1,5 @@
+# PROBLEM: Given a singly linked list, write a function which 
+# takes in the first node in a singly linked list and returns a boolean indicating if the linked list contains a "cycle".
+# A cycle is when a node's next point acutally points back to a previous node in the list.
+# This is also sometimes known as a circularly linked list.
+
